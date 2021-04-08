b916fa18000368b21fa7abba323748cde62ff63c
Pablo Ruiz <pabloruizsoria@gmail.com>
2021-04-08 11:14:51 +0200

    probando

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def get_moodle(subdomain):
 
 def run_moosh_command(moodle, command, capture=False):
     print("run_moosh_command(...)")
-    print("moodle: ", moodle)
+    print("command: '", command, "'")
     command_string = f"docker exec {moodle['container_name']} {command}"
     if capture:
         data = os.popen(command_string).read()
