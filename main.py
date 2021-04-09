a85ec29f9bcab72985dddb2499589ea69cc0c8ea
Pablo Ruiz <pabloruizsoria@gmail.com>
2021-04-09 10:50:48 +0200

    probando

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ def get_alumnos_moodle(moodle):
     Devuelve un objeto como el siguiente:
     #
     """
-    cmd = "moosh -n user-list -n 50" #listado de usuarios limitado a 50.000 # username (id), email,
+    cmd = "moosh -n user-list -n 50000" #listado de usuarios limitado a 50.000 # username (id), email,
     alumnosMoodle = run_moosh_command(moodle, cmd, True)
     print("alumnosMoodle: '",alumnosMoodle, "'" )
     
