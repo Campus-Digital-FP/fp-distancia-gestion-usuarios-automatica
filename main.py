0d094f0bf1bcd41470514e3e22ca0c9aedc0282a
Debian <debian@packer-output-300576bc-3972-41d0-9d63-efb675bbe346>
2021-04-19 13:10:10 +0200

    comienzo a hacer la desmatriculacion

--- main.py
+++ main.py
@@ -19,10 +19,12 @@ from classes.Ciclo import *
 from classes.Modulo import *
 
 def main():
+    #
+    usuarios_moodle_no_borrables = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] # ids de users creados en deploy que no hay que borrar
     # 
     moodle = get_moodle(SUBDOMAIN)[0]
     alumnosFicheroJson = []
-    alumnosMoodle = get_alumnos_moodle(moodle) # Alumnos que figuran en moodle antes de ejecutar el script
+    alumnosMoodle = get_alumnos_moodle_no_borrados(moodle) # Alumnos que figuran en moodle antes de ejecutar el script
     
     #########################################
     # Obtengo curso académico que debo usar
@@ -73,10 +75,9 @@ def main():
     alumnos_en_moodle_pero_no_SIGAD = [  ]
     for alumnoMoodle in alumnosMoodle:
         existe = False
-        # comprobamos si existe por dni/nie/... o si es profesor
+        # comprobamos si existe por dni/nie/...
         for alumnoSIGAD in alumnosFicheroJson:
-            if alumnoMoodle['username'].lower() == alumnoSIGAD.getDocumento().lower() \
-                    or alumnoMoodle['username'].lower().startswith("prof") :
+            if alumnoMoodle['username'].lower() == alumnoSIGAD.getDocumento().lower():
                 existe = True
                 break
         if not existe:
@@ -107,41 +108,116 @@ def main():
         if not existe:
             alumnos_a_borrar.append(alumnoMoodle)
 
-    usuarios_moodle_no_borrables = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] # ids de users creados en deploy que no hay que borrar
+    
     print("*** Alumnos a eliminar de Moodle")
     
     for alumnoMoodle in alumnos_a_borrar:
         print("- ", repr(alumnoMoodle) )
-        if alumnoMoodle['userid'] not in usuarios_moodle_no_borrables:
+        if alumnoMoodle['userid'] not in usuarios_moodle_no_borrables: # TODO: it's not working
             delete_alumno_moodle(alumnoMoodle, moodle)
         else:
-            print("NO se borra")
+            print("Alumno configurado como NO borrable")
     
     ########################
-    # Desmatriculo a aquellos alumnos que SIGAD me dice ya no deberían estar matriculados en un determinado curso
+    # Desmatriculo de Moodle a aquellos alumnos que SIGAD me dice ya no deberían estar matriculados en un determinado curso
     # Obtengo y recorro los usuarios de moodle. 
     # Itero sobre los alumnos y obtengo en qué están matriculados en moodle:
     # - si están matriculados en algo en que no estén matriculados en SIGAD los desmatriculo
     # excepto si el shortname del curso termina en t (módulo de tutoría del ciclo)
     ########################
-    alumnosMoodle = get_alumnos_moodle(moodle) # No me vale valor anterior pues contendrá alumnos
-    for alumnoMoodle in alumnosMoodle:
-        if not alumnoMoodle['username'].lower().startswith("prof"): # es alumno, lo proceso
-            # TODO
-            print("TODO: Habría que comprobar en que cursos está actualmente matriculado el usuario de moodle ", alumnoMoodle['username'], " (", alumnoMoodle['userid'] ,")" )
-            
+    alumnos_moodle = get_alumnos_moodle_no_borrados(moodle) 
+    cursos_moodle = get_cursos(moodle)
+
+    for alumno_moodle in alumnos_moodle:
+        userid = alumno_moodle['userid']
+        # no recorro los no borrables
+        if userid in usuarios_moodle_no_borrables: # TODO: no está funcionando
+            continue
+        username = alumno_moodle['username']
+
+        for curso in cursos_moodle:
+            courseid = curso['courseid']
+            course_shortname = curso['shortname']
+            print("Evaluando si el alumno", username, "está ahora matriculado en Moodle en el curso", course_shortname)
+            command = '''\
+            mysql --user=\"{DB_USER}\" --password=\"{DB_PASS}\" --host=\"{DB_HOST}\" -D \"{DB_NAME}\"  --execute=\"
+                SELECT u.id, u.username, u.email, c.id, c.shortname 
+                FROM mdl_user u 
+                INNER JOIN mdl_user_enrolments ue ON ue.userid = u.id 
+                INNER JOIN mdl_enrol e ON e.id = ue.enrolid 
+                INNER JOIN mdl_course c ON e.courseid = c.id
+                WHERE c.id={courseid} AND u.id = {userid}
+            \"
+            '''.format(DB_USER = DB_USER, DB_PASS = DB_PASS, DB_HOST = DB_HOST, DB_NAME = DB_NAME, courseid = courseid, userid = userid )
+            out = run_command( command , True).rstrip()
+            print("out: '", out, "'")
+            if out != "":
+                print("Actualmente el alumno", username, "está ahora matriculado en moodle en el curso", course_shortname, ". Vamos a comprobar si en SIGAD también está")
+                course_codes = course_shortname.split("-") # 0 centreid 1 siglas ciclo 2 codigo materia
+                print("course_codes[0]:", course_codes[0])
+                print("course_codes[1]:", course_codes[1])
+                print("course_codes[2]:", course_codes[2])
+                for alumno in alumnosFicheroJson:
+                    en_sigad_esta_matriculado = False
+                    if alumno.getDocumento().lower() == username.lower(): #he encontrado al alumno en SIGAD
+                        print("-", repr(alumno) )
+                        centros = alumno.getCentros()
+                        print("*Mirando centros del alumno")
+                        for centro in centros:
+                            if en_sigad_esta_matriculado:
+                                break
+                            if course_codes[0] == centro.get_codigo_centro(): #sigo profundizando
+                                ciclos = centro.getCiclos()
+                                print("**Mirando ciclos del alumno")
+                                for ciclo in ciclos:
+                                    if en_sigad_esta_matriculado:
+                                        break;
+                                    if course_codes[1] == ciclo.get_siglas_ciclo(): #sigo profundizando
+                                        modulos = ciclo.getModulos()
+                                        print("***Mirando módulos del alumno")
+                                        for modulo in modulos:
+                                            print("course_codes[2]:'", course_codes[2],"'" )
+                                            print("modulo.get_id_materia():'", modulo.get_id_materia(),"'" )
+                                            print("ID of course_codes[2] =", hex(id(course_codes[2])))
+                                            print("ID of modulo.get_id_materia() =", hex(id(modulo.get_id_materia())))
+                                            if en_sigad_esta_matriculado:
+                                                break
+                                            if course_codes[2] == modulo.get_id_materia(): #he llegado al módulo
+                                                en_sigad_esta_matriculado = True
+                                                print("El alumno si está matriculado en SIGAD en ese curso")
+                                    else:
+                                        continue;
+                            else:
+                                continue
+                        if not en_sigad_esta_matriculado:
+                            print("En SIGAD el alumno", username, "NO está matriculado en", course_shortname, "se procede a desmatricular de moodle")
+                            desmatricula_alumno_en_curso(moodle, userid, courseid)
+                            assert(False)
+                        break # una vez he procesado al alumno no tiene sentido seguir mirando los demás alumnos de SIGAD
+            # si el alumno no está matriculado en el curso de moodle no hacer nada mas con él
+            # si el alumno está matriculado en el curso de moodle entonces
+            #  - si SIGAD también dice que debe estar matriculado entonces no hacer nada
+            #  - si SIGAD dice que en ese curso no está matriculado entonces darle de baja
+    
+    assert(False)
 
     ########################
     # Proceso el fichero JSON (foto de SIGAD)
     # - si un alumno del fichero no existe en moodle lo creo
     # - matriculo a un alumno en los cursos que tenga asignados en SIGAD
     ########################
+    usuarios_no_creables = [ ]
     for alumno in alumnosFicheroJson:
-        # print("-", repr(alumno) )
+        print("*** Procesando alumno de fichero JSNON ***")
+        print("-", repr(alumno) )
         id_alumno = ""
         # Creo en moodle los alumnos que estén en el json y no estén en moodle
         if not existeAlumnoEnMoodle(moodle, alumno):
-            id_alumno = crearAlumnoEnMoodle(moodle, alumno)
+            try:
+                id_alumno = crearAlumnoEnMoodle(moodle, alumno)
+            except ValueError as e:
+                usuarios_no_creables.append(alumno)
+                continue
         else:
             id_alumno = get_id_alumno_by_dni(moodle, alumno)
         
@@ -154,11 +230,18 @@ def main():
                     id_materia = modulo.get_id_materia()
 
                     shortname_curso = str(codigo_centro) + "-" + str(siglas_ciclo) + "-" + str(id_materia)
-
-                    if not is_alumno_matriculado_en_curso(moodle, id_alumno, shortname_curso):
-                        id_curso = get_id_de_curso_by_shortname(moodle, shortname_curso)
+                    id_curso = get_id_de_curso_by_shortname(moodle, shortname_curso)
+                    if id_curso == "": # el curso no existe
+                        print("El curso ", shortname_curso , " no existe.")
+                    elif not is_alumno_matriculado_en_curso(moodle, id_alumno, id_curso):
                         matricula_alumno_en_curso(moodle, id_alumno, id_curso)
-                        
+                    else:
+                        print("El alumno (",id_alumno,") ya estaba matriculado en ", shortname_curso)
+    # Listo alumnos que no se han podido crear
+    # TODO: enviar por email
+    print("Alumnos de SIGAD que no se han podido crear en Moodle: ")
+    for alumno in usuarios_no_creables:
+        print( "- ", repr(alumno) )
     ########################
     # Envío email resumen de lo hecho por email a responsables
     ########################
@@ -208,9 +291,19 @@ def get_moodle(subdomain):
     return container
 
 def run_moosh_command(moodle, command, capture=False):
-    print("run_moosh_command(...)")
+    #print("run_moosh_command(...)")
     print("command: '", command, "'")
     command_string = f"docker exec {moodle['container_name']} {command}"
+    if capture:
+        data = os.popen(command_string).read()
+        return data
+    if not capture:
+        os.system(command_string)
+
+def run_command(command, capture=False):
+    print("run_command(...)")
+    print("command: '", command, "'")
+    command_string = command
     if capture:
         data = os.popen(command_string).read()
         return data
@@ -224,7 +317,7 @@ def desmatricula_alumno_en_curso(moodle, id_alumno, id_curso):
     print("desmatricula_alumno_en_curso(...)")
 
     cmd = "moosh -n course-unenrol " + id_curso + " " + id_alumno
-    out = run_moosh_command(moodle, cmd, False)
+    run_moosh_command(moodle, cmd, False)
 
 def matricula_alumno_en_curso(moodle, id_alumno, id_curso):
     """
@@ -233,7 +326,7 @@ def matricula_alumno_en_curso(moodle, id_alumno, id_curso):
     print("matricula_alumno_en_curso(...)")
 
     cmd = "moosh -n course-enrol -i " + id_curso + " " + id_alumno
-    out = run_moosh_command(moodle, cmd, False)
+    run_moosh_command(moodle, cmd, False)
 
 def get_id_alumno_by_dni(moodle, alumno):
     """
@@ -241,25 +334,31 @@ def get_id_alumno_by_dni(moodle, alumno):
     """
     print("get_id_alumno_by_dni(...)")
 
-    cmd = "moosh -n user-list | grep " + alumno.getDocumento().lower() + " | cut -d \",\" -f 1 | cut -d \"(\" -f 2 | sed 's/)//' "
+    cmd = "moosh -n user-list -n 50000 | grep " + alumno.getDocumento().lower() + " | cut -d \",\" -f 1 | cut -d \"(\" -f 2 | sed 's/)//' "
     id_alumno = run_moosh_command(moodle, cmd, True).rstrip()
     print("id_alumno: ", id_alumno)
     return id_alumno
 
-def is_alumno_matriculado_en_curso(moodle, id_alumno, shortname):
+def is_alumno_matriculado_en_curso(moodle, id_alumno, id_curso):
     """
     Dado un moodle, un id_alumno y un curso devuelve:
-    - true si el id_alumno existe en el curso
-    - false si el id_alumno no existe en el curso
+    - True si el id_alumno existe en el curso
+    - False si el id_alumno no existe en el curso
     """
     print("is_alumno_matriculado_en_curso(...)")
-    id_curso = get_id_de_curso_by_shortname(moodle, shortname)
 
     cmd = "moosh -n user-list --course " + id_curso + " | grep \"(" + id_alumno + ")\" "
-    out = run_moosh_command(moodle, cmd, True)
+    out = run_moosh_command(moodle, cmd, True).rstrip()
     print("out: ", out)
+    if out == "":
+        return False
+    else:
+        return True
 
 def send_email(destinatario, asunto, texto):
+    """
+    Al destinatario envía un email con el asunto y texto dados
+    """
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(SMTP_HOSTS, SMTP_PORT, context=context) as server:
@@ -295,6 +394,7 @@ def get_id_de_curso_by_shortname(moodle, shortname):
     """
     Dado un moodle y un shotname devuelve el id del curso si el mismo existe
     """
+    print("get_id_de_curso_by_shortname(...)")
     cmd = "moosh -n course-list \"shortname = '" + shortname + "'\" | tail -n 1 | cut -d \",\" -f1 | sed 's/\"//' | sed 's/\"//' " 
     course_info = run_moosh_command(moodle, cmd, True).rstrip()
 
@@ -302,13 +402,13 @@ def get_id_de_curso_by_shortname(moodle, shortname):
 
     return course_info
 
-def get_alumnos_moodle(moodle):
-    print("get_alumnos_moodle(...)")
+def get_alumnos_moodle_no_borrados(moodle):
+    print("get_alumnos_moodle_no_borrados(...)")
     """
-    Devuelve una lista de usuarios que actualmente están en moodle:
+    Devuelve una lista de alumnos (omite usuarios con username que empiece por prof) que actualmente están en moodle:
     #
     """
-    cmd = "moosh -n user-list -n 50000" #listado de usuarios limitado a 50.000 # username (id), email,
+    cmd = "moosh -n user-list -n 50000 \"deleted = 0 and username not like 'prof%' \" " #listado de usuarios limitado a 50.000 # username (id), email,
     alumnosMoodle = run_moosh_command(moodle, cmd, True)
     
     alumnos = []    
@@ -328,6 +428,35 @@ def get_alumnos_moodle(moodle):
     
 
     return alumnos
+
+def get_cursos(moodle):
+    print("get_alumnos_moodle_no_borrados(...)")
+    """
+    Devuelve una lista de los cursos que existen en moodle
+    #
+    """
+    cmd = "moosh -n course-list | tail -n +2" 
+    cursos_moodle = run_moosh_command(moodle, cmd, True)
+    
+    cursos = []    
+    
+    data_s = io.StringIO(cursos_moodle).read()
+    lines = data_s.splitlines()
+    curso = [
+        {
+            "courseid": line.split("\",\"")[0].replace("\"","").lstrip(),
+            "category": line.split("\",\"")[1].rstrip(),
+            "shortname": line.split("\",\"")[2].lstrip(),
+            "fullname": line.split("\",\"")[3].rstrip(),
+            "visible": line.split("\",\"")[4].replace("\"","").rstrip(),
+        }
+        for line in lines
+        # if line.split()[-1].endswith("moodle_1")
+    ]
+    cursos.extend(curso)
+    
+
+    return cursos    
 
 def procesaJsonEstudiantes(y, alumnosFicheroJson):
     """
@@ -435,17 +564,33 @@ def crearAlumnoEnMoodle(moodle, alumno):
     Devuelve el id del alumno creado
     """
     print("crearAlumnoEnMoodle(...)")
-    cmd = "moosh -n user-create --password changeme --email " + alumno.getEmail() \
-        + " --digest 2 --city Aragón --country ES --firstname \"" +  alumno.getNombre() \
-        + "\" --lastname \"" +  alumno.getApellidos() + "\" " \
-        + alumno.getDocumento().lower()
+    alumno_creable = True
+    if alumno.getEmail() is None:
+        print("El alumno a crear no tiene email")
+        alumno_creable = False
+    if alumno.getNombre() is None:
+        print("El alumno a crear no tiene nombre")
+        alumno_creable = False
+    if alumno.getApellidos() is None:
+        print("El alumno a crear no tiene apellidos")
+        alumno_creable = False
+    if alumno.getDocumento() is None:
+        print("El alumno a crear no tiene documento")
+        alumno_creable = False
 
-    idUser = run_moosh_command(moodle, cmd, True)
+    if alumno_creable:
+        cmd = "moosh -n user-create --password changeme --email " + alumno.getEmail() \
+            + " --digest 2 --city Aragón --country ES --firstname \"" +  alumno.getNombre() \
+            + "\" --lastname \"" +  alumno.getApellidos() + "\" " \
+            + alumno.getDocumento().lower()
 
-    print("idUser: '",idUser,"'")
-    print("idUser: '",int(idUser),"'")
+        idUser = run_moosh_command(moodle, cmd, True).rstrip()
 
-    return idUser
+        print("idUser: '",idUser,"'")
+
+        return idUser
+    else:
+        raise ValueError
     #
     # End of crearAlumnoEnMoodle
     #
